c27ecc5b4dbd37d2da40f22896108b1c578d0ce8
huangjunqi1 <1017728803@qq.com>
2019-04-25 20:59:14 +0800

    支持选择题目

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ import os
 import random
 import string
 from bottle import *
+from bottle import request, route, run, static_file
+
+value=None
+
+@route('/')
+def index():
+    return static_file('index.html', './')
 
 site = '192.68.4.124'
 
@@ -17,13 +24,17 @@ if not os.path.exists(upload_path):
 
 
 @route('/', method='GET')
+
+
 #@route('/upload', method='GET')
 #@route('/index.html', method='GET')
 #@route('/upload.html', method='GET')
 def index():
     return static_file('index.html', './')
-
+    
+   
 def judge(simp_name, problem_id):
+#    print('JUDGE ' + simp_name + ' ' + problem_id)
     print('cp ./upload/' + simp_name + '.cpp' + ' ' + './problems/' + problem_id + '/' + simp_name + '.cpp')
     os.system('cp ./upload/' + simp_name + '.cpp' + ' ' + './problems/' + problem_id + '/' + simp_name + '.cpp')
     print('cd ./problems/' + problem_id + ' && syzoj judge ' + simp_name + '.cpp > ' + simp_name + '.txt')
@@ -33,13 +44,17 @@ def judge(simp_name, problem_id):
     print('mv ./problems/' + problem_id + '/' + simp_name + '.txt' + ' ' + './upload/' + simp_name + '.txt')
     os.system('mv ./problems/' + problem_id + '/' + simp_name + '.txt' + ' ' + './upload/' + simp_name + '.txt')
 
+@route('/upload/command')
+def command():
+	global value  
+	value=request.query.value
+
 @route('/upload', method='POST')
 def do_upload():
     filedata = request.files.get('fileField')
-
     simp_name = ''.join(random.sample(string.ascii_letters + string.digits, 10))
     save_name = simp_name + '.cpp'
-    
+#    print(value)
     if filedata.file:
         file_name = os.path.join(upload_path, save_name)
         try:
@@ -47,7 +62,7 @@ def do_upload():
         except IOError:
             return '上传文件失败'
         print(save_name)
-        problem_id = '0'
+        problem_id = value
         judge(simp_name, problem_id)
         return '上传文件成功, 文件名: {}'.format(site + '/upload/' + save_name) + '评测结果：{}'.format(site + '/upload/' + simp_name + '.txt')
 
@@ -59,13 +74,10 @@ def do_upload():
 def show_code(file_name):
     print(file_name)
     return static_file(file_name, './upload/')
-"""
-@route('/favicon.ico', method='GET')
-def server_static():
-    return static_file('favicon.ico', root=base_path)
-"""
 
 
+
+    
 @error(404)
 def error404(error):
     return '404 发生页面错误, 未找到内容'
