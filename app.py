2917c414cf4fbbd1bc6e0710946c3d61f8a6fa4a
PhoenixGS <thestarrydream@gmail.com>
2019-04-24 21:36:01 -0700

    暂时改成上传文件

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
-from bottle import request, route, run, static_file
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-@route('/')
+
+import os
+from bottle import *
+
+
+base_path = os.path.dirname(os.path.realpath(__file__))  # 获取脚本路径
+
+upload_path = os.path.join(base_path, 'upload')   # 上传文件目录
+if not os.path.exists(upload_path):
+    os.makedirs(upload_path)
+
+
+@route('/', method='GET')
+#@route('/upload', method='GET')
+#@route('/index.html', method='GET')
+#@route('/upload.html', method='GET')
 def index():
     return static_file('index.html', './')
 
-@route('/command')
-def command():
-    value = request.query.value
-    print(value)
-    return 'Value was set to: ' + value
+
+@route('/upload', method='POST')
+def do_upload():
+    filedata = request.files.get('fileField')
+    
+    if filedata.file:
+        file_name = os.path.join(upload_path, filedata.filename)
+        print(filedata.filename)
+        try:
+            filedata.save(file_name)  # 上传文件写入
+        except IOError:
+            return '上传文件失败'
+        return '上传文件成功, 文件名: {}'.format(file_name)
+    else:
+        return '上传文件失败'
+
+"""
+@route('/favicon.ico', method='GET')
+def server_static():
+    return static_file('favicon.ico', root=base_path)
+"""
+
+
+@error(404)
+def error404(error):
+    return '404 发生页面错误, 未找到内容'
+
 
 run(host='0.0.0.0', port=8000, debug=True)
+
